74b76a0cddd09a511c5e5c7f7874ac309c08c5fb
peanutBana <diao0321@gmail.com>
2023-01-30 17:13:28 +0900

    first commit

---
+++ src/test/java/com/example/campingsite/CampingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
