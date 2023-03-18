ab9ab75004cedc0b36a72061c253bd3fe5c3b992
peanutBana <diao0321@gmail.com>
2023-03-18 23:51:25 +0900

    관리자 페이지 컨트롤러 작성

--- src/main/java/com/campingsite/controller/AdminController.java
+++ src/main/java/com/campingsite/controller/AdminController.java
@@ -1,17 +1,41 @@
 package com.campingsite.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import com.campingsite.entity.Camp;
+import com.campingsite.entity.Reservation;
+import com.campingsite.entity.User;
+import com.campingsite.repository.CampRepository;
+import com.campingsite.repository.ResvRepository;
+import com.campingsite.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping(value = "/admin")
 @RequiredArgsConstructor
-public class AdminController {
+public class AdminController {	
+	private final UserRepository userRepository;
+	private final ResvRepository resvRepository;
+	private final CampRepository campRepository;
+	
+	
 	@GetMapping(value="/setting")
-	public String loginMember() {
+	public String loginMember(Optional<Integer> page, Model model) {
+		List<User> users = userRepository.findAll();
+		List<Camp> camps = campRepository.findAll();
+		List<Reservation> reservations = resvRepository.findAll();
+
+		model.addAttribute("users", users);
+		model.addAttribute("camps", camps);
+		model.addAttribute("reservations", reservations);
+		
 		return "admin/setting";
 	}
 }
